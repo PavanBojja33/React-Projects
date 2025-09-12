397639da83c26df9ce767c12e2d276a2d3de7916
Pavan Bojja <pavanbojja33@gmail.com>
2025-09-12 22:13:02 +0530

    counter

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -7,9 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h2>Counter : {count}</h2>
-      button
+    <div className='main'>
+      <h2>Counter :</h2>
+      <h2>{count}</h2>
+      <div className="b1">
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+      </div>
+      <button onClick={()=>setCount(0)}>Reset</button>
     </div>
   )
 }
